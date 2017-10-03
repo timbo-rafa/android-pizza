59436c81e12fedd6386438aad6557d456bfd14bd
Rafael Timbó <timbo.rafa@gmail.com>
2017-10-02 21:25:39 -0400

    Company Image and Description

--- app/src/main/java/com/example/rafael/rafaelmatos_mapd711_lab_pizzaonline/LandingActivity.java
+++ app/src/main/java/com/example/rafael/rafaelmatos_mapd711_lab_pizzaonline/LandingActivity.java
@@ -10,4 +10,25 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
     }
+
+    public boolean onCreateOptionMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.speed, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle menu items using their id
+        switch (item.getItemId()){
+            case R.id.m1:
+                Toast.makeText("You selected option 1!").show();
+                break;
+            case R.id.m2:
+                break;
+            case R.id.m3:
+                break;
+            case R.id.m4:
+                break;
+        }
+    }
 }
