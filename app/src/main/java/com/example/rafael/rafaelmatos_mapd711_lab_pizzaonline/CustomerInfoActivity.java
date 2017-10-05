c8fde3863aa84ff494dfd0062d8c24f489abf67b
Rafael Timbó <timbo.rafa@gmail.com>
2017-10-04 22:45:40 -0400

    v0.23 Customer Info Screen Design

--- app/src/main/java/com/example/rafael/rafaelmatos_mapd711_lab_pizzaonline/CustomerInfoActivity.java
+++ app/src/main/java/com/example/rafael/rafaelmatos_mapd711_lab_pizzaonline/CustomerInfoActivity.java
@@ -1,13 +1,33 @@
 package com.example.rafael.rafaelmatos_mapd711_lab_pizzaonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CustomerInfoActivity extends AppCompatActivity {
+
+    EditText customerNameEditText;
+    EditText customerAddressEditText;
+    EditText customerZipEditText;
+    EditText customerPhoneEditText;
+    EditText customerCardEditText;
+    EditText customerCardExpiryDateEditText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_info);
+        Intent toppingsActivity = getIntent();
+
+        customerNameEditText = (EditText) findViewById(R.id.customerNameInput);
+        customerAddressEditText = (EditText) findViewById(R.id.customerAddressInput);
+        customerZipEditText = (EditText) findViewById(R.id.customerZipCodeInput);
+        customerPhoneEditText = (EditText) findViewById(R.id.customerPhoneInput);
+        customerCardEditText = (EditText) findViewById(R.id.customerCardInput);
+        customerCardExpiryDateEditText = (EditText) findViewById(R.id.customerCardExpiryDateInput);
     }
 }
